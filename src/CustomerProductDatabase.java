509fb8489719417b6a3abbe4233c17ee601d26a5
Youssef Amr Mohamed Abd Wareth <es.yousef.amr2023@alexu.edu.eg>
2025-10-19 20:50:39 +0300

    all classes in the src

---
+++ src/CustomerProductDatabase.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+
+public class CustomerProductDatabase extends DataBase<CustomerProduct> {
+
+    CustomerProductDatabase(String fileName) {
+        super(fileName);
+    }
+
+    @Override
+    public CustomerProduct createRecordFrom(String line){
+//        line=line.trim();
+        String[] fields = line.split(",");
+        String customerSSN = fields[0];
+        String productID = fields[1];
+        LocalDate purchaseDate = LocalDate.parse(fields[3]);
+        CustomerProduct cp = new CustomerProduct(customerSSN,productID,purchaseDate);
+        return cp;
+    }
+
+    @Override
+    public String getSearchKey(CustomerProduct record) {
+        return record.getSearchKey();
+    }
+
+    @Override
+    public String lineRepresentation(CustomerProduct record) {
+        return record.lineRepresentation();
+    }
+
+}
